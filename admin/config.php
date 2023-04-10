d6579bf8c07e530236f6a93a398c2b4b78469052
Serhii Kulynets <kylun1serg@gmail.com>
2023-04-10 10:31:25 +0300

    oc pinka/ add cart/catalog/product

---
+++ admin/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://pinka.in.ua/admin/');
+define('HTTP_CATALOG', 'https://pinka.in.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://pinka.in.ua/admin/');
+define('HTTPS_CATALOG', 'https://pinka.in.ua/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/pinka.in.ua/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/pinka.in.ua/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/pinka.in.ua/image/');
+define('DIR_STORAGE', 'C:/OpenServer/domains/pinka20storage/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/pinka.in.ua/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'pinka');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
---
+++ catalog/language/ru-ru/common/header.php
@@ -0,0 +1,22 @@
+<?php
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
+// Text
+$_['text_home']          = 'Главная';
+$_['text_wishlist']      = 'Закладки (%s)';
+$_['text_shopping_cart'] = 'Корзина';
+$_['text_category']      = 'Категории';
+$_['text_account']       = 'Личный кабинет';
+$_['text_register']      = 'Регистрация';
+$_['text_login']         = 'Авторизация';
+$_['text_order']         = 'История заказов';
+$_['text_transaction']   = 'Операции';
+$_['text_download']      = 'Файлы для скачивания';
+$_['text_logout']        = 'Выход';
+$_['text_checkout']      = 'Оформление заказа';
+$_['text_search']        = 'Поиск';
+$_['text_social']        = 'Мы в соцсетях:';
+$_['text_messages']      = 'Помощь и консультация:';
+$_['text_time']          = 'Ежедн. с 9:00 до 18:00';
+$_['text_phone']         = 'Позвонить';
